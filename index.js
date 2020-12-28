b691efadbaca24997a99e335213688e8b84dbad5
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-12-28 17:37:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ function unleashDog(dogName="Byron", dogBreed="poodle") {
 const routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
 
 function exerciseDog(dogName, dogBreed) {
-  return routine.map(funct => fn(dog, breed))
+  return routine.map(funct => funct(dogName, dogBreed))
 }
